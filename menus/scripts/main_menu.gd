4424473a4860e03361472f302840e906ac2e895f
Nah700 <nahimk30@gmail.com>
2025-11-26 16:05:11 +0900

    [ADD]main menu

---
+++ menus/scripts/main_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+func _ready():
+	%Play.pressed.connect(play)
+	%Quit.pressed.connect(quit_game)
+	%SettingsContainer.hide()
+
+func play():
+	get_tree().change_scene_to_file("res://debug.tscn")
+
+func quit_game():
+	get_tree().quit()
+
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value - 20)
+
+func _on_mute_volume_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0, toggled_on)
+
+func _on_resolution_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+
+func _on_full_screen_toggled(toggled_on: bool) -> void:
+	if (toggled_on):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
